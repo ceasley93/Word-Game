463e6cd891a480c0108a84f80e8f64aee30e53e8
JCP14 <JCP14@FSTU603007.pti.edu>
2019-02-14 11:48:06 -0500

    First Commit

---
+++ src/word/game/WordGame.java
@@ -0,0 +1,36 @@
+package word.game;
+
+public class WordGame {
+
+    // Variables
+    private String word;
+    private String hint;
+    
+    // Main Method - Used for getting input
+    public static void main(String[] args) {
+        
+        // Get Scanner variables here
+        
+        // Variables for guesses, input and boolean for compare returned value.
+        
+        // Loop until the game either runs out of guesses or the user gets the word correct.
+        
+        // Ifstatement
+        
+    }
+    
+    // Compare - Compares
+    public static boolean Compare(String userInput) {
+        
+        return false;
+        
+    }
+    
+    // Generate Word - Generates a random number anduses it as the index of an array.
+    public static void generateWord() {
+        
+        
+        
+    }
+    
+}
